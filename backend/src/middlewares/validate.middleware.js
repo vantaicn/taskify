13f3119c3db75bff4620c3ef977becf5802821f6
vantaicn <vantaicn.no1@gmail.com>
2025-07-25 09:22:40 +0700

    update validate - [body, query, params]

--- backend/src/middlewares/validate.middleware.js
+++ backend/src/middlewares/validate.middleware.js
@@ -1,11 +1,11 @@
-const validateMiddleware = (schema) => {
+const validateMiddleware = (schema, source = "body") => {
   return (req, res, next) => {
-    const { error } = schema.validate(req.body);
+    const { error } = schema.validate(req[source]);
     if (error) {
       return res.status(400).json({ error: error.details[0].message });
     }
     next();
   };
-}
+};
 
 module.exports = validateMiddleware;
