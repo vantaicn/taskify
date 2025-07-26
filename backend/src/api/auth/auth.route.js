2b2039d6ae550a093140019e8736b1c02134c0ad
vantaicn <vantaicn.no1@gmail.com>
2025-07-26 22:30:56 +0700

    update code format

--- backend/src/api/auth/auth.route.js
+++ backend/src/api/auth/auth.route.js
@@ -1,9 +1,9 @@
 const express = require('express');
+const router = express.Router();
 const authController = require('./auth.controller');
 const authValidation = require('./auth.validation');
 const validateMiddleware = require('../../middlewares/validate.middleware');
 
-const router = express.Router();
 
 router.post('/register', validateMiddleware(authValidation.registerRequest), authController.register);
 router.post('/login', validateMiddleware(authValidation.loginRequest), authController.login);
