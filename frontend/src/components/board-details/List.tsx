168b3184abd11f5a7f969c7a1346a0c1595cda33
vantaicn <vantaicn.no1@gmail.com>
2025-08-24 01:36:46 +0700

    update lists UI and logic: fetch lists, add scrollbar, blur 'Add new list' button

--- frontend/src/components/board-details/List.tsx
+++ frontend/src/components/board-details/List.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import Task from "./Task";
 import {
   Card,
@@ -10,28 +11,101 @@ import {
 } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { Plus } from "lucide-react";
-import type { List as ListType } from "@/types/list.types";
+import type { ListType } from "@/types/list.types";
+import type { TaskType } from "@/types/task.types";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
 
+import useTask from "@/hooks/useTask";
 
 export interface ListProps {
   list: ListType;
 }
 
-const List = ({list}: ListProps) => {
-  const tasks = list.tasks;
+const POSITION_GAP = 100;
+
+const List = ({ list }: ListProps) => {
+  const { useGetTasks, createTaskMutation } = useTask(list.boardId, list.id);
+  const tasksQuery = useGetTasks();
+  const tasks = tasksQuery.data || [];
+
+  const [newTaskData, setNewTaskData] = React.useState({
+    title: "",
+    description: "",
+    position: tasks.length > 0 ? tasks[tasks.length - 1].position + POSITION_GAP : 0,
+  });
+  const [isOpenNewTaskDialog, setIsOpenNewTaskDialog] = React.useState(false);
+
+  const handleCreateTask = async () => {
+    await createTaskMutation.mutateAsync(newTaskData);
+    setNewTaskData({ title: "", description: "", position: tasks.length > 0 ? tasks[tasks.length - 1].position + POSITION_GAP : 0 });
+    setIsOpenNewTaskDialog(false);
+  };
+
   return (
-    <Card className="w-64 bg-gray-100">
+    <Card className="min-w-[16rem] bg-gray-100 pb-1">
       <CardHeader>
-        <CardTitle>Task List</CardTitle>
+        <CardTitle>{list.title}</CardTitle>
       </CardHeader>
       <CardContent className="space-y-2 p-2">
-        {/* {tasks.map((task) => (
-          <Task key={task.id} task={task} />
-        ))} */}
-        <Button variant="outline" className="w-full mt-2">
-          <Plus />
-          Add Task
-        </Button>
+        <div className="space-y-2 max-h-[30rem] overflow-y-auto scrollbar-thin scrollbar-track-transparent scrollbar-thumb-transparent hover:scrollbar-thumb-gray-300">
+          {tasks?.map((task: TaskType) => (
+            <Task key={task.id} task={task} />
+          ))}
+        </div>
+        <Dialog
+          open={isOpenNewTaskDialog}
+          onOpenChange={setIsOpenNewTaskDialog}
+        >
+          <DialogTrigger asChild>
+            <Button variant="outline" className="w-full border-0 bg-white/30 hover:bg-white/50 text-gray-600 backdrop-blur-md">
+              <Plus className="w-4 h-4 mr-1" />
+              Add New Task
+            </Button>
+          </DialogTrigger>
+          <DialogContent>
+            <DialogHeader>
+              <DialogTitle>Add New Task</DialogTitle>
+              <DialogDescription>
+                Enter the details for the new task.
+              </DialogDescription>
+            </DialogHeader>
+            <Label htmlFor="task-title">Task Title</Label>
+            <Input
+              id="task-title"
+              placeholder="Enter task title"
+              value={newTaskData.title}
+              onChange={(e) =>
+                setNewTaskData({ ...newTaskData, title: e.target.value })
+              }
+            />
+            <Label htmlFor="task-description">Description</Label>
+            <Input
+              id="task-description"
+              placeholder="Enter task description"
+              value={newTaskData.description}
+              onChange={(e) =>
+                setNewTaskData({ ...newTaskData, description: e.target.value })
+              }
+            />
+            <DialogFooter>
+              <DialogClose asChild>
+                <Button variant="outline">Cancel</Button>
+              </DialogClose>
+              <Button onClick={handleCreateTask}>Create</Button>
+            </DialogFooter>
+          </DialogContent>
+        </Dialog>
       </CardContent>
     </Card>
   );
--- frontend/src/pages/BoardDetailsPage.tsx
+++ frontend/src/pages/BoardDetailsPage.tsx
@@ -3,36 +3,58 @@ import List from "@/components/board-details/List";
 import { MoreHorizontal, UserPlus, Plus } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { useParams } from "react-router-dom";
-import type { List as ListType } from "@/types/list.types";
+import type { ListType } from "@/types/list.types";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
 
-import useBoards from "@/hooks/useBoards";
+import useBoard from "@/hooks/useBoard";
 import useList from "@/hooks/useList";
 
 const POSITION_GAP = 100;
 
 const BoardDetailsPage = () => {
-  const { useGetBoardById } = useBoards();
+  const { useGetBoardById } = useBoard();
   const { boardId } = useParams();
   const boardQuery = useGetBoardById(boardId || "");
-
   const board = boardQuery.data;
-  const lists = board?.lists || [];
-  console.log("Board details:", board);
-  console.log("Lists:", lists);
 
-  const { createListMutation } = useList();
+  const { useGetLists, createListMutation } = useList(boardId || "");
+  const listsQuery = useGetLists();
+  const lists = listsQuery.data || [];
 
-  const [newList, setNewList] = React.useState({ title: "", boardId: boardId || "", position: lists.length > 0 ? lists[lists.length - 1].position + POSITION_GAP : 0 });
+  const [newList, setNewList] = React.useState({
+    title: "",
+    boardId: boardId || "",
+    position:
+      lists.length > 0 ? lists[lists.length - 1].position + POSITION_GAP : 0,
+  });
+  const [isOpenNewListDialog, setIsOpenNewListDialog] = React.useState(false);
 
   const handleAddListAsync = async () => {
     await createListMutation.mutateAsync(newList);
-    setNewList({ title: "", boardId: boardId || "", position: lists.length > 0 ? lists[lists.length - 1].position + POSITION_GAP : 0 });
+    setNewList({
+      title: "",
+      boardId: boardId || "",
+      position:
+        lists.length > 0 ? lists[lists.length - 1].position + POSITION_GAP : 0,
+    });
+    setIsOpenNewListDialog(false);
   };
 
   return (
-    <>
+    <div className="bg-gradient-to-br from-purple-300 via-purple-300 to-pink-400 min-h-screen">
       {/* Header */}
-      <div className="h-16 bg-gray-100 border-b border-gray-200 flex items-center justify-between px-4">
+      <div className="h-16 bg-black/30 backdrop-blur-md flex items-center justify-between px-4">
         <h1 className="text-lg font-semibold">{boardQuery.data?.title}</h1>
         <div className="item-end px-6 flex items-center gap-2">
           <Button variant="outline" className="text-sm text-gray-500">
@@ -46,16 +68,54 @@ const BoardDetailsPage = () => {
       </div>
 
       {/* Lists */}
-      <div className="flex space-x-4 p-4">
+      <div className="flex items-start gap-4 p-4 overflow-x-auto w-360
+                      scrollbar-thin 
+                      scrollbar-track-transparent
+                      scrollbar-thumb-transparent
+                      hover:scrollbar-thumb-gray-300">
         {lists.map((list: ListType) => (
           <List key={list.id} list={list} />
         ))}
-        <Button variant="outline" className="w-64" onClick={handleAddListAsync}>
-          <Plus className="w-4 h-4 mr-1" />
-          Add New List
-        </Button>
-      </div>    
-    </>
+
+        <Dialog
+          open={isOpenNewListDialog}
+          onOpenChange={setIsOpenNewListDialog}
+        >
+          <DialogTrigger asChild>
+            <Button
+              variant="outline"
+              className="min-w-[16rem] bg-white/30 hover:bg-white/50 text-gray-600 border-0"
+            >
+              <Plus className="w-4 h-4 mr-1" />
+              Add New List
+            </Button>
+          </DialogTrigger>
+          <DialogContent>
+            <DialogHeader>
+              <DialogTitle>Add New List</DialogTitle>
+              <DialogDescription>
+                Enter the title for the new list.
+              </DialogDescription>
+            </DialogHeader>
+            <Label htmlFor="list-title">List Title</Label>
+            <Input
+              id="list-title"
+              value={newList.title}
+              onChange={(e) =>
+                setNewList({ ...newList, title: e.target.value })
+              }
+              placeholder="Enter list title"
+            />
+            <DialogFooter>
+              <DialogClose asChild>
+                <Button variant="outline">Cancel</Button>
+              </DialogClose>
+              <Button onClick={handleAddListAsync}>Create</Button>
+            </DialogFooter>
+          </DialogContent>
+        </Dialog>
+      </div>
+    </div>
   );
 };
 
