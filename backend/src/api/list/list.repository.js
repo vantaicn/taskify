98ea4abf4e8b3f4b9ebd6962fe65fe277099e8a3
vantaicn <vantaicn.no1@gmail.com>
2025-08-29 12:56:47 +0700

    include ordered tasks when get lists

--- backend/src/api/list/list.repository.js
+++ backend/src/api/list/list.repository.js
@@ -2,27 +2,34 @@ const db = require("../../models/models");
 
 const createList = async (title, boardId, position) => {
   return await db.List.create({ title, boardId, position });
-}
+};
 
 const getListsByBoardId = async (boardId) => {
-  return await db.List.findAll({ where: { boardId }, order: [['position', 'ASC']] });
-}
+  return await db.List.findAll({
+    where: { boardId },
+    include: [{ model: db.Task, as: "tasks" }],
+    order: [
+      ["position", "ASC"],
+      [{ model: db.Task, as: "tasks" }, "position", "ASC"],
+    ],
+  });
+};
 
 const getListById = async (listId) => {
   return await db.List.findByPk(listId);
-}
+};
 
 const updateListTitle = async (listId, title) => {
   return await db.List.update({ title }, { where: { id: listId } });
-}
+};
 
 const updateListPosition = async (listId, position) => {
   return await db.List.update({ position }, { where: { id: listId } });
-}
+};
 
 const deleteListById = async (listId) => {
   return await db.List.destroy({ where: { id: listId } });
-}
+};
 
 module.exports = {
   createList,
@@ -31,4 +38,4 @@ module.exports = {
   updateListTitle,
   updateListPosition,
   deleteListById,
-}
+};
